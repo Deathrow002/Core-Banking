4658c7636a89c3d33e1884831a1e6f9e49fdf8d7
Mr Back Ally Cat <krittamateza@gmail.com>
2025-01-10 15:23:57 +0700

    Update AccountApplication and TransactionApplication for Services not found Discovery by hind DiscoveryServiceChecker

--- Account/src/main/java/com/account/AccountApplication.java
+++ Account/src/main/java/com/account/AccountApplication.java
@@ -1,26 +1,15 @@
 package com.account;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import com.account.config.DiscoveryServiceChecker;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
-//    @Autowired
-//    private DiscoveryServiceChecker discoveryServiceChecker;
-
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
 
-//    @PostConstruct
-//    public void init() {
-//        discoveryServiceChecker.waitForDiscoveryService();
-//    }
 }
--- Transaction/src/main/java/com/transaction/TransactionApplication.java
+++ Transaction/src/main/java/com/transaction/TransactionApplication.java
@@ -1,9 +1,5 @@
 package com.transaction;
 
-import com.transaction.config.DiscoveryServiceChecker;
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,15 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableCaching
 public class TransactionApplication {
 
-//    @Autowired
-//    private DiscoveryServiceChecker discoveryServiceChecker;
-
     public static void main(String[] args) {
         SpringApplication.run(TransactionApplication.class, args);
     }
-
-//    @PostConstruct
-//    public void init() {
-//        discoveryServiceChecker.waitForDiscoveryService();
-//    }
 }
