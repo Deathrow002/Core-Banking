f43ad7859312593af58174e7d51ebe97c72e29b7
Mr Back Ally Cat <krittamateza@gmail.com>
2025-03-28 10:54:56 +0700

    Change Configuration for Kubernetes, docker-compose and Dockerfile.

--- Discovery/src/main/java/com/discov/DiscoveryServerApplication.java
+++ Discovery/src/main/java/com/discov/DiscoveryServerApplication.java
@@ -1,13 +1,1 @@
-package com.discov;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-@SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
-    }
-}
+// File removed as Discovery Service is merged into Gateway Service.
