fb957b3617e40bc15b9a475f6106be845e8e263d
Mr Back Ally Cat <krittamateza@gmail.com>
2025-06-18 09:25:52 +0700

    refactor: remove redundant Javadoc comments from JwtService methods

--- Authentication/src/main/java/com/authentication/service/JwtService.java
+++ Authentication/src/main/java/com/authentication/service/JwtService.java
@@ -23,21 +23,10 @@ public class JwtService {
 
     private final JwtConfig jwtConfig;
 
-    /**
-     * Generate a token
-     * @param userDetails
-     * @return
-     */
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    /**
-     * Generate a token
-     * @param extraClaims
-     * @param userDetails
-     * @return
-     */
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
                 .setClaims(extraClaims)
@@ -49,61 +38,28 @@ public class JwtService {
                 .compact();
     }
 
-    /**
-     * Check if the token is valid
-     * @param token
-     * @param userDetails
-     * @return
-     */
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    /**
-     * Extract the username from the token
-     * @param token
-     * @return
-     */
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    /**
-     * Check if the token is expired
-     * @param token
-     * @return
-     */
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    /**
-     * Extract the expiration date from the token
-     * @param token
-     * @return
-     */
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    /**
-     * Extract a specific claim from the token
-     * @param token
-     * @param claimsResolver
-     * @param <T>
-     * @return
-     */
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    /**
-     * Extract all claims from the token
-     * @param token
-     * @return
-     */
     private Claims extractAllClaims(String token) {
         byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.getSecretKey());
         return Jwts
